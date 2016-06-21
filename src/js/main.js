ff3d7f8487de0555a6f2d486df09f7c6907cfd9d
kolialike <kolialike@gmail.com>
2016-06-21 10:14:29 +0300

    commit #2

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,15 @@
 jQuery(function($){
-   $('#bb-bookblock').bookblock({
-   	nextEl      : '.bb-nav-prev',
-   	circular    : true,
-   	prevEl      : '.bb-nav-next'
-   });
-   
+   $('#bb-bookblock').imagesLoaded( function() {
+      $('#bb-bookblock').bookblock({
+       	nextEl      : '.bb-nav-prev',
+       	circular    : true,
+       	prevEl      : '.bb-nav-next'
+       });
+      $(window).resize(function () {
+        $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+      });
       $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
-
-    $('.bb-bookblock .bb-item img').imagesLoaded( function() {
-  		$('.bb-bookblock').css('height', $(".bb-bookblock img").height());
-  	});
+   });    
 
 });
 
-  $(window).resize(function () {
-    $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
-    $('.bb-bookblock').imagesLoaded( function() {
-  		$('.bb-bookblock').css('height', $(".bb-bookblock img").height());
-    });
-	
-});
