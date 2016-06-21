6fc7b6bb25b9f551b31720a2482a45b858b127e4
kolialike <kolialike@gmail.com>
2016-06-21 09:52:54 +0300

    comit

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,19 @@ jQuery(function($){
    	circular    : true,
    	prevEl      : '.bb-nav-next'
    });
+   
+      $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
 
-   $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+    $('.bb-bookblock .bb-item img').imagesLoaded( function() {
+  		$('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+  	});
 
 });
 
-$(window).resize(function () {
-   $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+  $(window).resize(function () {
+    $('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+    $('.bb-bookblock').imagesLoaded( function() {
+  		$('.bb-bookblock').css('height', $(".bb-bookblock img").height());
+    });
 	
 });
--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -2,4 +2,5 @@
 //= jquery-1.11.3.min.js
 //= jquerypp.custom.js
 //= jquery.bookblock.js
+//= imagesloaded.pkgd.min.js
 
